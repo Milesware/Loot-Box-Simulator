3c5bb5e1892cb74d7beef98663e5d825cb421a67
DESKTOP-HF2S5R4\jiami <jiamingy@usc.edu>
2017-12-17 19:42:33 -0800

    1 thing you can still do after NN is gone

---
+++ Project Lootbox/Assets/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float Health;
+    public GameObject Sword;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GetHealth() <= 0)
+        {
+            Destroy(this.gameObject);
+            Destroy(Sword);
+        }
+	}
+
+    public float GetHealth()
+    {
+        return Health;
+    }
+
+    public void DecrementHealth(float decrease)
+    {
+        Health -= decrease;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Sword" & other.gameObject != Sword)
+        {
+            DecrementHealth(Time.deltaTime * 20);
+        }
+    }
+}
---
+++ Project Lootbox/Assets/Script/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rig;
+    public string HorizontalInput;
+    public string VerticalInput;
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHori = Input.GetAxis(HorizontalInput);
+        float moveVert = Input.GetAxis(VerticalInput);
+        Vector3 movement = new Vector3(moveHori,0,-moveVert);
+        rig.velocity = movement * speed;
+        
+        
+	}
+}
