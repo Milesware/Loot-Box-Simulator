91e68c228c4ddc86df81c08fce791a93741e5347
DESKTOP-HF2S5R4\jiami <jiamingy@usc.edu>
2018-02-20 21:58:09 -0800

    Single player input finished for touch

--- Project Lootbox/Assets/Script/PlayerMove.cs
+++ Project Lootbox/Assets/Script/PlayerMove.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 
     public float speed;
+    private Touch mTouch;
     public string HorizontalInput;
     public string VerticalInput;
-
-    private Touch mTouch;
-    private Vector3 lastPosition;
+    
     private Rigidbody rig;
     // Use this for initialization
 
     void Start () {
         rig = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch t in Input.touches)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.name == gameObject.name)
+                    {
+                        mTouch = t;
+                        Debug.Log("User tapped on game object " + hit.collider.gameObject.name);
+                    }
+                }
+                if (t.phase == TouchPhase.Moved)
+                {
+                    Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(t.position.x, t.position.y, 10));
+                    touchedPos.y = 1;
+                    AttemptMove(touchedPos - transform.position);
+                }
+            }
+            
+
+        }
+        
+    }
+
+    void FixedUpdate () {
         Vector3 Movement;
 
 //#if UNITY_STANDALONE || UNITY_WEBPLAYER
@@ -26,34 +55,9 @@ public class PlayerMove : MonoBehaviour {
         float moveHori = Input.GetAxis(HorizontalInput);
         float moveVert = Input.GetAxis(VerticalInput);
         Movement = new Vector3(moveHori,0,-moveVert);
-        
-        if (Input.touchCount > 0)
-        {
-            foreach(Touch t in Input.touches)
-            {
-                if (t.phase == TouchPhase.Began)
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(t.position);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        Debug.Log("User tapped on game object " + hit.collider.gameObject.name);
-                        if (hit.collider.gameObject == this)
-                        {
-                            mTouch = t;
-                            lastPosition = hit.transform.position;
-                        }
-                    }
-                }
-                if (t.phase == TouchPhase.Moved)
-                {
-                    Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(mTouch.position.x, mTouch.position.y, 10));
-                    transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
 
-                }
-            }
-            
-        }
+        
+
 
     }
 
