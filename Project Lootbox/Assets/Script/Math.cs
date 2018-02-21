30ca102e1ae9706cb1f1d714f5f26bbfc512aae8
DESKTOP-HF2S5R4\jiami <jiamingy@usc.edu>
2018-02-20 21:02:15 -0800

    New version of the game

---
+++ Project Lootbox/Assets/Script/Math.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Math  {
+
+    private static float mTolerance = 0.5f;
+
+    public static bool isCloseEnuf(Vector2 a, Vector2 b)
+    {
+        Vector2 diff = a - b;
+        return (diff.x < mTolerance && diff.y < mTolerance);
+    }
+}
